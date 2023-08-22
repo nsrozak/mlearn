c44eb834d133cc04c9c33353fe7ff127ba43f78c
nsrozak <natalie.s.rozak@gmail.com>
2023-08-22 18:13:08 -0400

    Add classification example

--- examples/classification.py
+++ examples/classification.py
@@ -2,9 +2,9 @@
 
 # global imports
 import scipy.stats as stats
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingClassifier
 
-from mlearn.datasets import SyntheticRegression
+from mlearn.datasets import SyntheticClassification
 from mlearn.preprocessors import MLTrainPreprocessor, MLPreprocessor
 from mlearn.modelers import MLTrainModel, MLModel
 
@@ -12,11 +12,13 @@ from mlearn.modelers import MLTrainModel, MLModel
 random_state = 123
 
 # training function arguments
-train_make_regression_kwargs = {'n_samples': 1000, 
-                                'n_features': 12,
-                                'n_informative': 12,
-                                'random_state': 123
-                               }
+train_make_classification_kwargs = {'n_samples': 1000, 
+                                    'n_features': 12,
+                                    'n_informative': 12,
+                                    'n_redundant': 0,
+                                    'n_repeated': 0,
+                                    'random_state': 123
+                                   }
 continuous_range = [(-100, 100), (10, 20), (0, 50), (-5, 5), 
                     (-5, 60), (100, 2000), (4, 90), (10, 12), (0, 70)
                     ]
@@ -32,32 +34,35 @@ scaler_columns = ['feature_0', 'feature_1', 'feature_2', 'feature_3', 'feature_4
 scaler_kwargs = {}
 scaler_type = 'robust'
 
-max_features = 1 / 3
-classification = False
+max_features = 'sqrt'
+classification = True
 n_splits = 5
 n_iter = 50
-scoring = 'neg_root_mean_squared_error'
+scoring = 'balanced_accuracy'
 param_distributions = {'max_depth': stats.randint(1, 10),
                        'min_samples_leaf': stats.randint(1, 10),
-                       'n_estimators': stats.randint(50, 250),
+                       'n_estimators': stats.randint(25, 150),
                        'learning_rate': stats.uniform(0.001, 0.5)
                       }
 
 # testing function arguments
-test_make_regression_kwargs = {'n_samples': 100, 
-                               'n_features': 12,
-                               'n_informative': 12,
-                               'random_state': 456
-                              }
+test_make_classification_kwargs = {'n_samples': 100, 
+                                   'n_features': 12,
+                                   'n_informative': 12,
+                                   'n_redundant': 0,
+                                   'n_repeated': 0,
+                                   'random_state': 456
+                                  }
 
 ### Training ###
 
 # generate synthetic data
-train_synthetic_regression = SyntheticRegression(make_regression_kwargs=train_make_regression_kwargs,
-                                                 continuous_range=continuous_range, 
-                                                 categories_number=categories_number
-                                                )
-train_data = train_synthetic_regression.get_data()
+train_synthetic_classification = SyntheticClassification(
+    make_classification_kwargs=train_make_classification_kwargs,
+    continuous_range=continuous_range, 
+    categories_number=categories_number
+)
+train_data = train_synthetic_classification.get_data()
 
 # print current summary statistics
 print('Summary statistics of original train data')
@@ -88,7 +93,7 @@ print('Summary statistics of validation data')
 print(val.describe())
 
 # train the model
-model = GradientBoostingRegressor(max_features=max_features, random_state=random_state)
+model = GradientBoostingClassifier(max_features=max_features, random_state=random_state)
 ml_train_model = MLTrainModel(model, 
                               classification=classification, 
                               n_splits=n_splits, 
@@ -103,11 +108,12 @@ y_val_hat, val_score = ml_train_model.predict(X_val, y_val)
 ### Testing ###
 
 # generate synthetic data
-test_synthetic_regression = SyntheticRegression(make_regression_kwargs=test_make_regression_kwargs,
-                                                continuous_range=continuous_range, 
-                                                categories_number=categories_number
-                                               )
-test_data = test_synthetic_regression.get_data()
+test_synthetic_classification = SyntheticClassification(
+    make_classification_kwargs=test_make_classification_kwargs,
+    continuous_range=continuous_range, 
+    categories_number=categories_number
+)
+test_data = test_synthetic_classification.get_data()
 
 # print current summary statistics
 print('Summary statistics of original test data')
